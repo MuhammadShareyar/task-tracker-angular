eebec422852de4692b60c09d9dd937a0a11e9d7e
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2025-05-14 14:58:04 +0200

    setup http client and db server

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../../interfaces/task';
-import { TASKS } from '../../mock-task';
-import { TaskItemComponent } from "../task-item/task-item.component";
+import { TaskItemComponent } from '../task-item/task-item.component';
+import { TaskService } from '../../services/task.service';
 
 @Component({
   selector: 'app-tasks',
@@ -9,6 +9,12 @@ import { TaskItemComponent } from "../task-item/task-item.component";
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
-export class TasksComponent {
-  tasks: Task[] = TASKS;
+export class TasksComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private readonly tasksService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasksService.getTasks().subscribe((tasks) => (this.tasks = tasks));
+  }
 }
---
+++ src/app/services/task.service.ts
@@ -0,0 +1,18 @@
+import { inject, Injectable } from '@angular/core';
+import { Task } from '../interfaces/task';
+import { Observable} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TaskService {
+  private url = 'http://localhost:5000/tasks';
+  private http = inject(HttpClient);
+
+  constructor() {}
+
+  getTasks(): Observable<Task[]> {
+    return this.http.get<Task[]>(this.url);
+  }
+}
